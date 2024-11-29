b4a4c78099c2fa08dee4cb2f6d934e7d8f6245bb
Abhijit Roy <abhijit.x.roy@hotmail.com>
2024-11-29 10:43:41 +0530

    Initial commit

---
+++ src/main/java/com/maven/jenkins/maven_basic_project/MavenBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maven.jenkins.maven_basic_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenBasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenBasicProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/jenkins/maven_basic_project/MavenBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.jenkins.maven_basic_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenBasicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
